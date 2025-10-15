025e90422732e8cac9056f1756b0d0396d1f2ceb
nehadabdelgafour <nknehad100@gmail.com>
2025-10-15 16:30:34 +0300

    2Initial commit

---
+++ src/test/java/org/example/stepdefs/hooks.java
@@ -0,0 +1,19 @@
+
+    package org.example.stepdefs;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.cucumber.java.Before;
+import io.cucumber.java.After;
+
+    public class hooks {
+
+        public static WebDriver driver;
+        @Before
+        public int k0(){
+            return 0;
+        }
+
+
+
+    }
